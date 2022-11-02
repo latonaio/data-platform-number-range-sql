98440c0a9f1d8dd9b2765c92de7b6111883ee970
MiyuYokoyama0109 <miyu.y@latona.io>
2022-11-02 18:41:07 +0900

    release

--- data-platform-number-range-sql-latest-number-data.sql
+++ data-platform-number-range-sql-latest-number-data.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `data_platform_number_range_latest_number_data`
 (
-    `NumberRangeID`             varchar(2) NOT NULL,
+    `NumberRangeID`             varchar(5) NOT NULL,
     `ServiceLabel`              varchar(50) NOT NULL,
     `FieldNameWithNumberRange`  varchar(100) NOT NULL,
-    `LatestNumber`              int(12) DEFAULT NULL,
+    `LatestNumber`              int(16) DEFAULT NULL,
     PRIMARY KEY (`NumberRangeID`, `ServiceLabel`, `FieldNameWithNumberRange`),
-    CONSTRAINT `DataPlatformNumberRangeLatestNumberData_fk` FOREIGN KEY (`NumberRangeID`, `ServiceLabel`, `FieldNameWithNumberRange`) REFERENCES `data_platform_number_range_number_range_data` (`NumberRangeID`, `ServiceLabel`, `FieldNameWithNumberRange`),
+    CONSTRAINT `DataPlatformNumberRangeLatestNumberData_fk` FOREIGN KEY (`NumberRangeID`, `ServiceLabel`, `FieldNameWithNumberRange`) REFERENCES `data_platform_number_range_number_range_data` (`NumberRangeID`, `ServiceLabel`, `FieldNameWithNumberRange`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
--- data-platform-number-range-sql-number-range-data.sql
+++ data-platform-number-range-sql-number-range-data.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `data_platform_number_range_number_range_data`
 (
-    `NumberRangeID`             varchar(2) NOT NULL,
+    `NumberRangeID`             varchar(5) NOT NULL,
     `ServiceLabel`              varchar(50) NOT NULL,
     `FieldNameWithNumberRange`  varchar(100) NOT NULL,
-    `NumberRangeFrom`           int(12) DEFAULT NULL,
-    `NumberRangeTo`             int(12) DEFAULT NULL,
+    `NumberRangeFrom`           int(16) DEFAULT NULL,
+    `NumberRangeTo`             int(16) DEFAULT NULL,
     PRIMARY KEY (`NumberRangeID`, `ServiceLabel`, `FieldNameWithNumberRange`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
