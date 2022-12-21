619fb344e8ef9671cef4ad2322925ca0a06bafda
MiyuYokoyama0109 <miyu.y@latona.io>
2022-12-21 16:03:36 +0900

    release

--- data-platform-number-range-sql-latest-number-data.sql
+++ data-platform-number-range-sql-latest-number-data.sql
@@ -4,7 +4,10 @@ CREATE TABLE `data_platform_number_range_latest_number_data`
     `ServiceLabel`              varchar(50) NOT NULL,
     `FieldNameWithNumberRange`  varchar(100) NOT NULL,
     `LatestNumber`              int(16) DEFAULT NULL,
+
     PRIMARY KEY (`NumberRangeID`, `ServiceLabel`, `FieldNameWithNumberRange`),
+
     CONSTRAINT `DataPlatformNumberRangeLatestNumberData_fk` FOREIGN KEY (`NumberRangeID`, `ServiceLabel`, `FieldNameWithNumberRange`) REFERENCES `data_platform_number_range_number_range_data` (`NumberRangeID`, `ServiceLabel`, `FieldNameWithNumberRange`)
+
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
--- data-platform-number-range-sql-number-range-data.sql
+++ data-platform-number-range-sql-number-range-data.sql
@@ -3,8 +3,10 @@ CREATE TABLE `data_platform_number_range_number_range_data`
     `NumberRangeID`             varchar(5) NOT NULL,
     `ServiceLabel`              varchar(50) NOT NULL,
     `FieldNameWithNumberRange`  varchar(100) NOT NULL,
-    `NumberRangeFrom`           int(16) DEFAULT NULL,
-    `NumberRangeTo`             int(16) DEFAULT NULL,
+    `NumberRangeFrom`           int(16) NOT NULL,
+    `NumberRangeTo`             int(16) NOT NULL,
+    
     PRIMARY KEY (`NumberRangeID`, `ServiceLabel`, `FieldNameWithNumberRange`)
+
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
