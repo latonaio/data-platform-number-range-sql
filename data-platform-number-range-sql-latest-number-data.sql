15e87192926be40736a2a8ad86fb9ca914db994e
kimura <creative@latona.io>
2023-05-18 14:48:58 +0900

    release

--- data-platform-number-range-sql-latest-number-data.sql
+++ data-platform-number-range-sql-latest-number-data.sql
@@ -8,6 +8,6 @@ CREATE TABLE `data_platform_number_range_latest_number_data`
     PRIMARY KEY (`NumberRangeID`, `ServiceLabel`, `FieldNameWithNumberRange`),
 
     CONSTRAINT `DataPlatformNumberRangeLatestNumberData_fk` FOREIGN KEY (`NumberRangeID`, `ServiceLabel`, `FieldNameWithNumberRange`) REFERENCES `data_platform_number_range_number_range_data` (`NumberRangeID`, `ServiceLabel`, `FieldNameWithNumberRange`)
-
+    
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
